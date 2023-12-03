2502ea9cb08bb8d0498ba0c791535e09fbc5eba9
GussWare <gussware@gmail.com>
2023-12-02 19:52:38 -0600

    chore: se agregaron urls para login y user

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+from app.views.system.auth.login_view import LoginView
+from app.views.system.auth.register_user_view import RegisterUserView
+from app.views.system.users.user_view import UserPagination, UserList, UserDetail, UserEnabled, UserDisabled
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/v1/auth/login/', LoginView.as_view(), name='login_token'),
+    path('api/v1/auth/refresh-token/', jwt_views.TokenRefreshView.as_view(), name='refresh-token'),
+    path('api/v1/auth/register/', RegisterUserView.as_view(), name='register_user'),
+
+    path('api/v1/users/', UserList.as_view(), name='user-list'),
+    path('api/v1/users/pagination/', UserPagination.as_view(), name='user-pagination'),
+    path('api/v1/users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('api/v1/users/<int:pk>/enabled/', UserEnabled.as_view(), name='user-enabled'),
+    path('api/v1/users/<int:pk>/disabled/', UserDisabled.as_view(), name='user-disabled'),
 ]
